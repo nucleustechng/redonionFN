81c0876dd8eda0e7aa4ad3cb1f58a3321542750d
Matthew Hans-Bello <matthewhansbello@live.com>
2023-08-26 17:48:26 +0100

    Touches2

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,8 @@ import axios from "axios";
 export default axios.create({
   // baseURL: "https://red-onion-server-v2-6381301c4bdf.herokuapp.com/api/v2",
   // baseURL: "https://redonion-server.herokuapp.com/api/v2",
-  baseURL: "http://164.92.177.125:8080/api/v2",
+  //baseURL: "http://164.92.177.125:8080/api/v2",
+  baseURL: "api.redonion.io/api/v2",
+
 });
 
