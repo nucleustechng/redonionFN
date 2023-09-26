e53b8e4672a097c77ca6c6d43b6e145a64610fa2
Matthew Hans-Bello <matthewhansbello@live.com>
2023-09-26 18:51:25 +0100

    KYC and More3

--- src/Pages/Complaint/DataArea/TableArea.js
+++ src/Pages/Complaint/DataArea/TableArea.js
@@ -151,7 +151,7 @@ const TableArea = () => {
                   color="secondary"
                   fontWeight={600}
                 >
-                  Complaint
+                  Complaints
                 </Typography>
               </Box>
               <Stack
